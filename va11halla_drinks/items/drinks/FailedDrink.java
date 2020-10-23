59b77349d26795afdc184ff24d18d43258fa07a1
LaPapesse <68287723+LaPapesse02@users.noreply.github.com>
2020-10-24 00:07:16 +0200

    Delete FailedDrink.java

--- va11halla_drinks/items/drinks/FailedDrink.java
+++
@@ -1,9 +0,0 @@
-package com.github.lapapesse02.va11halla_drinks.items.drinks;
-
-import com.github.lapapesse02.va11halla_drinks.items.BaseDrinkItem;
-
-public class FailedDrink extends BaseDrinkItem {
-    public FailedDrink(Settings settings) {
-        super(settings, new int[] {0, 0, 0, 0, 0}, "failed_drink");
-    }
-}
