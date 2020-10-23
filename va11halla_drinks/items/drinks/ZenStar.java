644e606816cd5c513a3c253a9964b9129ff5a487
LaPapesse <68287723+LaPapesse02@users.noreply.github.com>
2020-10-24 00:11:23 +0200

    Delete ZenStar.java

--- va11halla_drinks/items/drinks/ZenStar.java
+++
@@ -1,9 +0,0 @@
-package com.github.lapapesse02.va11halla_drinks.items.drinks;
-
-import com.github.lapapesse02.va11halla_drinks.items.BaseDrinkItem;
-
-public class ZenStar extends BaseDrinkItem {
-    public ZenStar(Settings settings) {
-        super(settings, new int[] {4, 4, 4, 4, 4}, "zen_star");
-    }
-}
