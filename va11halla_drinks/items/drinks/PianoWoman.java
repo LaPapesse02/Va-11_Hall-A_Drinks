6f75a0673b61cfe8361920bb2bf04f364761a719
LaPapesse <68287723+LaPapesse02@users.noreply.github.com>
2020-10-24 00:09:14 +0200

    Delete PianoWoman.java

--- va11halla_drinks/items/drinks/PianoWoman.java
+++
@@ -1,9 +0,0 @@
-package com.github.lapapesse02.va11halla_drinks.items.drinks;
-
-import com.github.lapapesse02.va11halla_drinks.items.BaseDrinkItem;
-
-public class PianoWoman extends BaseDrinkItem {
-    public PianoWoman(Settings settings) {
-        super(settings, new int[] {5, 5, 2, 3, 3}, "piano_woman");
-    }
-}
