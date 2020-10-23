98af306c6848d33e10f02205f9a6b0b2f09dd1a5
LaPapesse <68287723+LaPapesse02@users.noreply.github.com>
2020-10-24 00:08:53 +0200

    Delete Moonblast.java

--- va11halla_drinks/items/drinks/Moonblast.java
+++
@@ -1,9 +0,0 @@
-package com.github.lapapesse02.va11halla_drinks.items.drinks;
-
-import com.github.lapapesse02.va11halla_drinks.items.BaseDrinkItem;
-
-public class Moonblast extends BaseDrinkItem {
-    public Moonblast(Settings settings) {
-        super(settings, new int[] {6, 0, 1, 1, 2}, "moonblast");
-    }
-}
