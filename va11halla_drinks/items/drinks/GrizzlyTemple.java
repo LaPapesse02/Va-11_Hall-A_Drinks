d699ea9bcc94fcc6d5d90711ed7e057a21aefce6
LaPapesse <68287723+LaPapesse02@users.noreply.github.com>
2020-10-24 00:08:06 +0200

    Delete GrizzlyTemple.java

--- va11halla_drinks/items/drinks/GrizzlyTemple.java
+++
@@ -1,9 +0,0 @@
-package com.github.lapapesse02.va11halla_drinks.items.drinks;
-
-import com.github.lapapesse02.va11halla_drinks.items.BaseDrinkItem;
-
-public class GrizzlyTemple extends BaseDrinkItem {
-    public GrizzlyTemple(Settings settings) {
-        super(settings, new int[] {3, 3, 3, 0, 1}, "grizzly_temple");
-    }
-}
