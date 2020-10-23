76f2248b6eb35fb385fe3ce0bc9ded98067fb64b
LaPapesse <68287723+LaPapesse02@users.noreply.github.com>
2020-10-24 00:05:14 +0200

    Delete BadTouch.java

--- va11halla_drinks/items/drinks/BadTouch.java
+++
@@ -1,9 +0,0 @@
-package com.github.lapapesse02.va11halla_drinks.items.drinks;
-
-import com.github.lapapesse02.va11halla_drinks.items.BaseDrinkItem;
-
-public class BadTouch extends BaseDrinkItem {
-    public BadTouch(Settings settings) {
-        super(settings, new int[] {0, 2, 2, 2, 4}, "bad_touch");
-    }
-}
