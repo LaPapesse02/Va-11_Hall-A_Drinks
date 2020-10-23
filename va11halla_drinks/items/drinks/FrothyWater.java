8a14c33e4a3750cfac811255bf81cea87e21b751
LaPapesse <68287723+LaPapesse02@users.noreply.github.com>
2020-10-24 00:07:55 +0200

    Delete FrothyWater.java

--- va11halla_drinks/items/drinks/FrothyWater.java
+++
@@ -1,9 +0,0 @@
-package com.github.lapapesse02.va11halla_drinks.items.drinks;
-
-import com.github.lapapesse02.va11halla_drinks.items.BaseDrinkItem;
-
-public class FrothyWater extends BaseDrinkItem {
-    public FrothyWater(Settings settings) {
-        super(settings, new int[] {1, 1, 1, 1, 0}, "frothy_water");
-    }
-}
