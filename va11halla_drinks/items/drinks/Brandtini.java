c4edc657c0dd1daaf58c2c6c3ace8cdcce836635
LaPapesse <68287723+LaPapesse02@users.noreply.github.com>
2020-10-24 00:06:16 +0200

    Delete Brandtini.java

--- va11halla_drinks/items/drinks/Brandtini.java
+++
@@ -1,9 +0,0 @@
-package com.github.lapapesse02.va11halla_drinks.items.drinks;
-
-import com.github.lapapesse02.va11halla_drinks.items.BaseDrinkItem;
-
-public class Brandtini extends BaseDrinkItem {
-    public Brandtini(Settings settings) {
-        super(settings, new int[] {6, 0, 3, 0, 1}, "brandtini");
-    }
-}
