ee3b215ae6a0e5a55451b041e3f1fa0b812374cf
LaPapesse <68287723+LaPapesse02@users.noreply.github.com>
2020-10-24 00:11:01 +0200

    Delete SunshineCloud.java

--- va11halla_drinks/items/drinks/SunshineCloud.java
+++
@@ -1,9 +0,0 @@
-package com.github.lapapesse02.va11halla_drinks.items.drinks;
-
-import com.github.lapapesse02.va11halla_drinks.items.BaseDrinkItem;
-
-public class SunshineCloud extends BaseDrinkItem {
-    public SunshineCloud(Settings settings) {
-        super(settings, new int[] {2, 2, 0, 0, 0}, "sunshine_cloud");
-    }
-}
